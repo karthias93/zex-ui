c0141267da9a9616b38fe3801c35ec505b775930
aravinthTharmalingam <aravintht302@gmail.com>
2022-12-30 01:26:57 +0530

    dfgfg

--- src/components/trade-page/trade-page-tab/position-content/PositionContent.jsx
+++ src/components/trade-page/trade-page-tab/position-content/PositionContent.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Space, Table, Tag } from 'antd';
+import { Button, Space, Table, Tag } from 'antd';
 function PositionContent(props) {
     const columns = [
         {
             title: 'Market',
             dataIndex: 'market',
-            key: 'name',
+            key: 'market',
             render: (text) => <a>{text}</a>,
         },
         {
@@ -14,34 +14,34 @@ function PositionContent(props) {
             key: 'size',
         },
         {
+            title: 'Net Value',
+            dataIndex: 'netvalue',
+            key: 'netvalue',
+        },
+        {
             title: 'Collateral',
             dataIndex: 'collateral',
             key: 'collateral',
         },
         {
-            title: 'Net Value',
-            dataIndex: 'net-value',
-            key: 'net-value',
-        },
-        {
             title: 'Entry Price',
-            dataIndex: 'entry-price',
-            key: 'ntry-price',
+            dataIndex: 'entryprice',
+            key: 'ntryprice',
         },
         {
             title: 'Mark Price',
-            dataIndex: 'mark-price',
-            key: 'mark-price',
+            dataIndex: 'markprice',
+            key: 'markprice',
         },
         {
             title: 'Liq. Price',
-            dataIndex: 'liq-price',
-            key: 'liq-price',
+            dataIndex: 'liqprice',
+            key: 'liqprice',
         },
         {
             title: 'PNL & ROE',
-            dataIndex: 'pnl-roe',
-            key: 'pnl-roe',
+            dataIndex: 'pnlroe',
+            key: 'pnlroe',
         },
         {
             title: 'Actions',
@@ -50,32 +50,40 @@ function PositionContent(props) {
         },
     ];
     const data = [
-        // {
-        //     key: '1',
-        //     name: 'John Brown',
-        //     age: 32,
-        //     address: 'New York No. 1 Lake Park',
-        //     tags: ['nice', 'developer'],
-        // },
-        // {
-        //     key: '2',
-        //     name: 'Jim Green',
-        //     age: 42,
-        //     address: 'London No. 1 Lake Park',
-        //     tags: ['loser'],
-        // },
-        // {
-        //     key: '3',
-        //     name: 'Joe Black',
-        //     age: 32,
-        //     address: 'Sidney No. 1 Lake Park',
-        //     tags: ['cool', 'teacher'],
-        // },
+        {
+            key: '1',
+            market: <>
+                <div className='text-white'>MTIC</div>
+                <div className='text-green-500 text-xs'>Long</div>
+            </>,
+            size: <>
+                <div className='text-white'>0.099 MATIC</div>
+                <div className='tp text-xs'>$0.077</div>
+            </>,
+            netvalue: <div className='text-white'>$0.55</div>,
+            collateral: <>
+                <div className='text-white'>0.099 MATIC</div>
+                <div className='tp text-xs'>$0.077</div>
+            </>,
+            entryprice: <div className='text-white'>$0.55</div>,
+            markprice: <div className='text-white'>$0.55</div>,
+            liqprice: <div className='text-white'>$0.55</div>,
+            pnlroe: <>
+                <div className='text-white'>Pending</div>
+                <div className='tp text-xs'>Filling Order</div>
+            </>,
+            actions:<div><Button className='zex-btn-sec'>Cancel</Button></div>
+
+        },
+
     ];
 
     return (
         <div className=''>
-            <Table className='zex-table' columns={columns} dataSource={data} />
+            <Table className='zex-table' columns={columns} dataSource={data}
+                scroll={{
+                    x: 200,
+                }} />
         </div>
     );
 }
